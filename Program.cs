d2fe53df5d954365d04a51c670379cb5a29b0800
Gabriel Marchetti dee Souza <gbielmarch@gmail.com>
2023-03-29 10:44:51 -0300

    media

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Entrada
+
+Console.WriteLine($"Digite a nota do primeiro aluno");
+float nota1 = float.Parse(Console.ReadLine());
+Console.WriteLine($"Digite a nota do segundo aluno");
+float nota2 = float.Parse(Console.ReadLine());
+Console.WriteLine($"Digite a nota do terceiro aluno");
+float nota3 = float.Parse(Console.ReadLine());
+Console.WriteLine($"Digite a nota do quarto aluno");
+float nota4 = float.Parse(Console.ReadLine());
+Console.WriteLine($"Digite a nota do quinto aluno");
+float nota5 = float.Parse(Console.ReadLine());
+
+float media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+Console.WriteLine($"sua media é:{media}");
+
